3728d965954f767841a688170f00e33d2846e9cb
Raghavendra <raghavendra@vivatechrnd.com>
2024-06-05 00:16:40 +0530

    updated

--- temple-trust/src/Components/Navbar/NonAuthenticatedNavbar/index.tsx
+++ temple-trust/src/Components/Navbar/NonAuthenticatedNavbar/index.tsx
@@ -13,14 +13,32 @@ const NonAuthenticatedNavbar = () => {
             </div>
             <div style={{display: "flex", gap:"1rem"}}>
                 <div>
-                    <Link to="/account-details">Account Details</Link>
+                    <Link to="/">Home</Link>
                 </div>
                 <div>
+                    <Link to="/about">About Us</Link>
+                </div>
+                <div>
+                    <Link to="/product">Product</Link>
+                </div>
+                <div>
+                    <Link to="/service">Service</Link>
+                </div>
+                <div>
+                    <Link to="/review">Review</Link>
+                </div>
+                <div>
+                    <Link to="/contact-us">Contact Us</Link>
+                </div>
+                <div>
+                    <Link to="/account-details">Account Details</Link>
+                </div>
+                {/* <div>
                     <Link to="/sign-up">Sign Up</Link>
                 </div>
                 <div>
                     <Link to="/sign-in">Sign In</Link>
-                </div>
+                </div> */}
             </div>
         </div>
     );
--- temple-trust/src/Routes/AuthenticationRoutes/index.tsx
+++ temple-trust/src/Routes/AuthenticationRoutes/index.tsx
@@ -4,12 +4,22 @@ import HomePage from "../../Pages/HomePage";
 import SingUp from "../../Pages/SignUp";
 import SingIn from "../../Pages/SingIn";
 import AccountDetails from "../../Pages/AccountDetails.tsx";
+import Product from "../../Pages/Product";
+import ContactUs from "../../Pages/ContactUs";
+import About from "../../Pages/About";
+import Service from "../../Pages/Service";
+import Review from "../../Pages/Review";
 
 const AuthenticationRoutes = () => {
     const routes = [
         { path: "/", element: <HomePage/> },
-        { path: "/sign-up", element: <SingUp/> },
-        { path: "/sign-in", element: <SingIn/> },
+        { path: "/about", element: <About/> },
+        { path: "/product", element: <Product/> },
+        { path: "/service", element: <Service/> },
+        { path: "/review", element: <Review/> },
+        { path: "/contact-us", element: <ContactUs/> },
+        // { path: "/sign-up", element: <SingUp/> },
+        // { path: "/sign-in", element: <SingIn/> },
         { path: "/account-details", element: <AccountDetails/> },
         { path: "*", element: <HomePage/> },
     ];
