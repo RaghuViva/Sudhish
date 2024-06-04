3d0c56a07d1a3c724f26c00f283a033d232b2ca8
sudhish <sudhishhegdec@gmail.com>
2024-06-04 20:24:39 +0530

    change

---
+++ temple-trust/src/AccountDetails.js
@@ -0,0 +1,8 @@
+// AccountDetails.js
+import React from 'react';
+
+function AccountDetails() {
+  return <h1>Account Details Page</h1>;
+}
+
+export default AccountDetails;
---
+++ temple-trust/src/Service.js
@@ -0,0 +1,8 @@
+// Service.js
+import React from 'react';
+
+function Service() {
+  return <h1>Service Page</h1>;
+}
+
+export default Service;
